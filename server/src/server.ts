ffdba1a056ed01804da3df3dd153ddfb2cba949f
Erik <erikpersson0884@gmail.com>
2026-04-05 18:48:13 +0200

    added job to automaticly delete old events

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import { startDeleteOldEventsJob } from "./jobs/deleteOldEvents.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
+
+startDeleteOldEventsJob();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
