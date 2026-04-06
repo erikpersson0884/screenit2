0ea990eab4f8811ccd92d6ad3e39efe85171a343
Erik <erikpersson0884@gmail.com>
2026-04-06 23:50:27 +0200

    added request option to include chalerms.it events

--- server/src/controllers/eventController.ts
+++ server/src/controllers/eventController.ts
@@ -6,21 +6,25 @@ import createEventService from "../services/eventService.js";
 import { AuthenticatedRequest } from "../types/AuthenticatedRequest.js";
 import { EventsResponseSchema, EventResponseSchema } from "../models/dtos/EventDTO.js";
 import { getChalmersITEvents } from "../repositories/chalmersITRepository.js";
+import "dotenv/config.js";
 
 const defaultEventService = createEventService();
 
-export const createEventController = (eventService: IEventService = defaultEventService): IEventController => ({
+const createEventController = (eventService: IEventService = defaultEventService): IEventController => ({
 
     getAllEvents: async (req, res) => {
         const events: Event[] = await eventService.getAllEvents();
-        const chalmersITEvents: Event[] = await getChalmersITEvents();
-        events.push(...chalmersITEvents);
+        const includeChalmersIT: boolean = req.query.includeChalmersIT !== "false";
+        if (includeChalmersIT) {
+            const chalmersITEvents: Event[] = await getChalmersITEvents();
+            events.push(...chalmersITEvents);
+        }
         sendValidatedResponse(res, EventsResponseSchema, events);
     },
 
     createEvent: async (req: AuthenticatedRequest, res) => {
         const user: User = req.user;
-        const { name, date } = req.body;
+        const { name, date }: {name: string, date: Date} = req.body;
 
         if (!req.file) throw new Error("File was not uploaded");
         const imagePath: string = req.file.filename;
@@ -37,15 +41,14 @@ export const createEventController = (eventService: IEventService = defaultEvent
 
     updateEvent: async (req: AuthenticatedRequest, res) => {
         const user: User = req.user as User;
-        const id = req.params.id as string;
+        const id: string = req.params.id as string;
 
         const eventToUpdate: Event | null = await eventService.getEventById(id);
         if (!eventToUpdate) throw new Error(`Event with id ${id} not found so it cannot be updated`);
 
         if (user.id !== eventToUpdate.createdById || user.role == "admin") throw new Error("Only admins or the creator of the event can update it");
 
-        const { name, date } = req.body;
-        const eventData = { name, date };
+        const eventData: {name: string, date: Date} = req.body;
         const updatedEvent: Event | null = await eventService.updateEvent(id, eventData);
         sendValidatedResponse(res, EventResponseSchema, updatedEvent ? updatedEvent : {});
     },
@@ -54,7 +57,7 @@ export const createEventController = (eventService: IEventService = defaultEvent
         const user: User = req.user as User;
         const id = req.params.id as string;
                              
-        const event = await eventService.getEventById(id);
+        const event: Event | null = await eventService.getEventById(id);
         if (!event) throw new Error(`Event with id ${id} not found so it cannot be deleted`);
 
         if (user.id !== event.createdById || user.role == "admin") throw new Error("Only admins can delete events");
